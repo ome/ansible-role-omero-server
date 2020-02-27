a57e4673f56bbb1e8d8d918f7fefc5385d471bf3
Simon Li <spli@dundee.ac.uk>
2020-02-27 19:00:35 +0000

    Support Ubuntu 18.04

--- molecule/resources/tests/test_python3.py
+++ molecule/resources/tests/test_python3.py
@@ -8,7 +8,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 OMERO = '/opt/omero/server/OMERO.server/bin/omero'
 # Need to match 5.6.dev2
 # VERSION_PATTERN = re.compile('(\d+)\.(\d+)\.(\d+)-ice36-')
-VERSION_PATTERN = re.compile('(\d+)\.(\d+)\.(\w+)')
+VERSION_PATTERN = re.compile(r'(\d+)\.(\d+)\.(\w+)')
 
 
 def test_omero_version(host):
